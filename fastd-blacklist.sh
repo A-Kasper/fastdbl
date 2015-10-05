8c14a7148317e9dcda18ba6a4ebe1f6a578bf9da
A.Kasper <ein.kasper@gmx.de>
2015-10-05 03:10:44 +0200

    add fastd-blacklist.local for local blacklisting

--- fastd-blacklist.sh
+++ fastd-blacklist.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 PEER_KEY=$1
 
 if /bin/grep -Fq $PEER_KEY /etc/fastd/fastd-blacklist.json; then
-	exit 1
+        exit 1
 else
-	exit 0
+        if /bin/grep -Fq $PEER_KEY /etc/fastd/fastd-blacklist.local; then
+                exit 1
+        else
+                exit 0
+        fi
 fi
